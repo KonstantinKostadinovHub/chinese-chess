a8c2416ddd92f67046a0a74af478ce0265de62c4
Boris Kotsev <bobiko2319abv@gmail.com>
2022-07-01 17:18:54 +0300

    more code changes

--- chinese-chess/include/ConfigManager.h
+++ chinese-chess/include/ConfigManager.h
@@ -17,7 +17,4 @@ public:
 
 	static SDL_Texture* m_pawn1;
 	static SDL_Texture* m_pawn2;
-	//static Entity* m_enityModelPlayer2;
-	//static Entity* m_enityModelEnemy;
-	//static Entity* m_enityModel;
 };
--- chinese-chess/include/Engine.h
+++ chinese-chess/include/Engine.h
@@ -12,7 +12,3 @@
 
 SDL_Texture* LoadTexture(string configFile, SDL_Renderer* renderer);
 bool MouseIsInRect(int2 coor, SDL_Rect rect);
-bool collRectRect(SDL_Rect a, SDL_Rect b);
-bool checkInOffBounds(SDL_Rect rect, int screenWidth, int screenHeight);
-float lerp(float start, float target, float f);
-int2 centerOfRect(SDL_Rect rect);
